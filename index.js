95c3871e07528096b7b8d203a73aaeb261d1dcea
trionNemesis <trionnemesis@trionNemesiss-MacBook-Pro.local>
2018-06-11 10:51:18 +0800

    add scripts format

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const BufferStreams = require('bufferstreams');
 const PluginError = require('plugin-error');
 module.exports = function (options) {
   let customWidth = 500;
-  if (options && options.maxScreenWidth) {
-    customWidth = options.maxScreenWidth
+  if (options && options.maxWidth) {
+    customWidth = options.maxWidth
   }
   return new Transform({
     objectMode: true,
@@ -13,25 +13,28 @@ module.exports = function (options) {
         cb(null, file);
         return;
       }
-      const replacer = '<script>var customWidth = innerWidth;\n' +
-        '    if (innerWidth > ' + customWidth + ') {\n' +
-        '      customWidth = ' + customWidth + '\n' +
-        '    }\n' +
-        '    document.documentElement.style.fontSize = 100 * customWidth / 750 + \'px\';\n' +
-        '    addEventListener(\'load\', function () {\n' +
-        '      setTimeout(function () {\n' +
-        '        document.documentElement.style.fontSize = 100 * customWidth / 750 + \'px\';\n' +
-        '        window.unit = 100 * customWidth / 750;\n' +
-        '        var e = document.createEvent(\'Event\');\n' +
-        '        e.initEvent(\'adjustReady\', true, true);\n' +
-        '        window.dispatchEvent(e);\n' +
-        '      }, 480);\n' +
-        '    });\n' +
-        '    addEventListener(\'orientationchange\', function () {\n' +
-        '      setTimeout(function () {\n' +
-        '        document.documentElement.style.fontSize = 100 * customWidth / 750 + \'px\';\n' +
-        '      }, 480)\n' +
-        '    });</script></head>';
+      const replacer = '  <script>\n' +
+        '   var customWidth = innerWidth;\n' +
+        '   if (innerWidth > ' + customWidth + ') {\n' +
+        '     customWidth = ' + customWidth + '\n' +
+        '   }\n' +
+        '   document.documentElement.style.fontSize = 100 * customWidth / 750 + \'px\';\n' +
+        '   addEventListener(\'load\', function () {\n' +
+        '     setTimeout(function () {\n' +
+        '       document.documentElement.style.fontSize = 100 * customWidth / 750 + \'px\';\n' +
+        '       window.unit = 100 * customWidth / 750;\n' +
+        '       var e = document.createEvent(\'Event\');\n' +
+        '       e.initEvent(\'adjustReady\', true, true);\n' +
+        '       window.dispatchEvent(e);\n' +
+        '     }, 480);\n' +
+        '   });\n' +
+        '   addEventListener(\'orientationchange\', function () {\n' +
+        '     setTimeout(function () {\n' +
+        '       document.documentElement.style.fontSize = 100 * customWidth / 750 + \'px\';\n' +
+        '     }, 480)\n' +
+        '   });\n' +
+        '  </script>\n' +
+        '</head>';
 
       function pxToRem(buffer, done) {
         let buffers;
